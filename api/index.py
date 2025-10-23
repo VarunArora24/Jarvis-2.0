56f80198a36addd3f93619c4baa4a3e8590827a4
VarunArora24 <varunarora242424@gmail.com>
2025-10-23 19:12:25 +0530

    deploying

--- api/index.py
+++ api/index.py
@@ -1,16 +1,12 @@
 from flask import Flask, render_template
-from werkzeug.middleware.proxy_fix import ProxyFix
+from serverless_wsgi import handle_request
 
 app = Flask(__name__)
-app.wsgi_app = ProxyFix(app.wsgi_app)
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
 # Vercel entry point
-def handler(environ, start_response):
-    from werkzeug.wrappers import Request, Response
-    request = Request(environ)
-    response = Response(app.full_dispatch_request())
-    return response(environ, start_response)
+def handler(event, context):
+    return handle_request(app, event, context)
