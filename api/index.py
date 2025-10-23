6c167d0314a91140ef5430e06bf86aa40a9ae769
VarunArora24 <varunarora242424@gmail.com>
2025-10-23 19:20:10 +0530

    deploying

--- api/index.py
+++ api/index.py
@@ -1,12 +1,7 @@
 from flask import Flask, render_template
-from serverless_wsgi import handle_request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
-
-# Vercel entry point
-def handler(event, context):
-    return handle_request(app, event, context)
